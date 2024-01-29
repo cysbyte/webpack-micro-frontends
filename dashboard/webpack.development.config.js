fcc7dc5ee2c9ffef9b9d27c84e1188901293c23c
chenyunshu2 <chenyunshu2@gmail.com>
2024-01-29 19:17:10 +0800

    create navigation bar for dashboard

--- dashboard/webpack.development.config.js
+++ dashboard/webpack.development.config.js
@@ -36,6 +36,12 @@ module.exports = {
                     }
                 }
             },
+            {
+                test: /\.scss/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            }
         ]
     },
     plugins: [
